1918f20806ecbd3ac45be3140204209bc47a7c55
AdriPanG <adripang92@gmail.com>
2017-06-25 14:11:44 +0200

    Modificación de código RiosNavegables

--- Arboles/RiosNavegables/Source.cpp
+++ Arboles/RiosNavegables/Source.cpp
@@ -5,49 +5,41 @@
 
 using namespace std;
 
-int cuencaNavegables(const Arbin<int>& a, int &navegables, int tramo) {
+int riosNavegables(const Arbin<int>& a, int &navegables, int tramos) {
 	if (a.esVacio()) {
-		tramo = 0;
+		tramos = 0;
 	}
 	else {
 		if (a.hijoIz().esVacio() && a.hijoDer().esVacio()) {
-			tramo = 1;
+			tramos = 1;
 		}
 
-		int tramoIz = 0, tramoDer = 0;
+		int tramosIz = 0, tramosDer = 0;
 
-		tramoIz = cuencaNavegables(a.hijoIz(), navegables, tramo);
-		tramoDer = cuencaNavegables(a.hijoDer(), navegables, tramo);
+		tramosIz = riosNavegables(a.hijoIz(), navegables, tramosIz);
+		tramosDer = riosNavegables(a.hijoDer(), navegables, tramosDer);
 
-		tramo = tramoIz + tramoDer + a.raiz();
+		tramos += tramosIz + tramosDer + a.raiz();
 
-		if (tramo < 0) {
-			tramo = 0;
+		if (tramos < 0) {
+			tramos = 0;
 		}
 
-		if (tramo >= 3) {
+		if (tramos >= 3) {
 			navegables++;
 		}
 	}
 
-	return tramo;
+	return tramos;
 }
 
-// Devuelve el número de tramos navegables en una cuenca.
-// Parámetros:
-	// cuenca: La cuenca representada como un árbol binario
-// Resultado:
-	// Número de afluentes navegables en la cuenca
-// Precondición: En 'cuenca' hay una representación válida de
-// una cuenca fluvial (esta precondición no es
-// necesario comprobarla)
 int numeroTramosNavegables(const Arbin<int>& cuenca) {
 	// A IMPLEMENTAR
-	int navegables = 0, tramo = 0;
-
-	tramo = cuencaNavegables(cuenca, navegables, tramo);
-
-	if (tramo >= 3) {
+	int tramos = 0, navegables = 0;
+	
+	tramos = riosNavegables(cuenca, navegables, tramos);
+	
+	if (tramos >= 3) {
 		navegables--;
 	}
 
