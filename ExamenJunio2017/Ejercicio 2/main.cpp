3611dc55b4a9e2172bcf1e7ced74919f92bf3bc2
AdriPanG <adripang92@gmail.com>
2017-09-08 19:08:32 +0200

    Ejercicio 2 de Árboles
    Hallar la cantidad de nodos singulares (suma de los nodos ancestros y
    descendientes)

---
+++ ExamenJunio2017/Ejercicio 2/main.cpp
@@ -0,0 +1,132 @@
+/**
+  NOMBRE Y APELLIDOS:
+  LABORATORIO:
+  PUESTO:
+  USUARIO DE DOMJUDGE USADO EN EL EXAMEN:
+*/
+
+
+#include "Arbin.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+/**
+  COMPLEJIDAD: Explica aquí justificadamente cuál es la complejidad de 
+               esta función
+			   ***A COMPLETAR***
+
+	0(n log n) ya que se recorre el arbol entero.
+*/
+
+unsigned int singularesAux(Arbin<int> a, int &suma, int sumaAnt) {
+	if (a.esVacio()) {
+		return 0;
+	}
+	else {
+		unsigned int singulares = 0, singularesIz = 0, singularesDer = 0;
+		int sumaIz = 0, sumaDer = 0;
+
+		singularesIz = singularesAux(a.hijoIz(), sumaIz, sumaAnt + a.raiz());
+		singularesDer = singularesAux(a.hijoDer(), sumaDer, sumaAnt + a.raiz());
+
+		if (sumaAnt == sumaIz + sumaDer) {
+			singulares++;
+		}
+
+		suma = a.raiz() + sumaIz + sumaDer;
+
+		return singulares + singularesIz + singularesDer;
+	}
+}
+
+unsigned int numero_singulares(Arbin<int> a) {
+	// A IMPLEMENTAR
+	int suma = 0, sumaAnt = 0;
+	return singularesAux(a, suma, sumaAnt);
+}
+
+Arbin<int> lee_arbol(istream& in) {
+	char c;
+	in >> c;
+	switch (c) {
+	case '#': return Arbin<int>();
+	case '[': {
+		int raiz;
+		in >> raiz;
+  		in >> c;
+		return Arbin<int>(raiz);
+	}
+	case '(': {
+		Arbin<int> iz = lee_arbol(in);
+		int raiz;
+		in >> raiz;
+		Arbin<int> dr = lee_arbol(in);
+		in >> c;
+		return Arbin<int>(iz, raiz, dr);
+	}
+	default: return Arbin<int>();
+	}
+}
+
+/**
+Este programa lee árboles de la entrada estándar, 
+codificados de acuerdo con las siguientes reglas:
+(1) El árbol vacío se codifica como #
+(2) Los árboles simples se codifican como [v], con  
+    v el valor del nodo
+(3) Los árboles complejos se codifican como (IvD),
+    con I la codificación del hijo izquierdo,
+	v el valor de la raíz, y D la codificación
+	del hijo derecho.
+Para cada árbol leído, escribe en la salida estádar
+el número de nodos singulares de dicho árbol.
+	
+Algunos casos representativos:
+
+#
+[0]
+[5]
+(([4]3[-3])1[-4])
+
+-El primero representa el árbol vacío. Al no tener nodos,
+un árbol vacío tiene 0 nodos sigulares.
+-El segundo representa un árbol con un único nodo, cuyo valor
+es 0. Dicho nodo es singular, ya que: (i) como no tiene 
+ascestros, la suma de los ancestros es 0, (ii) como no
+tiene descendientes, la suma de los descendientes es 0. 
+-El tercero representa otro árbol con un único nodo, cuyo
+valor es 5. Por la misma razón que en el segundo ejemplo,
+este nodo es singular.
+-El cuarto representa el siguiente árbol:
+     
+	       1
+		  /  \
+		  3  -4
+         / \  
+        4  -3 
+
+Este árbol tiene 2 nodos sigulares: la raíz, y la raíz del hijo 
+izquierdo. 
+
+Por tanto, la salida del programa para estos casos será:
+
+0
+1
+1
+2
+
+*/
+
+int main() {
+	Arbin<int> arbol;
+	while (cin.peek() != EOF)  {
+		cout << numero_singulares(lee_arbol(cin));
+		string resto_linea;
+		getline(cin, resto_linea);
+		if (cin.peek() != EOF) cout << endl;
+	}
+	return 0;
+}
